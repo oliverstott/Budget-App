fd1646054ec7afcf6250a33bf3611b50af765f5a
Oliver <ors@oliver-stott.co.uk>
2018-07-26 22:18:21 +0100

    Setting up the Module View Pattern IIFE

---
+++ starter/app.js
@@ -0,0 +1,14 @@
+//BUDGET CONTROLLER
+var budgetController = (function (){
+    //Code
+})();
+
+//UI CONTROLLER
+var UIController = (function (){
+    //Code
+})();
+
+//GLOBAL APP CONTROLLER
+var controller = (function (budgetCtrl, UICtrl){
+    //Code
+})(budgetController,UIController);
