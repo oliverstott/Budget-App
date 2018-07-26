d5a045da7203a5f1b38a0b30eda420d303e87ff6
Oliver <ors@oliver-stott.co.uk>
2018-07-26 23:32:49 +0100

    Added KeyPress event listener

--- starter/app.js
+++ starter/app.js
@@ -10,5 +10,33 @@ var UIController = (function (){
 
 //GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl){
-    //Code
+   
+        
+var ctrlAddItem = function(){
+    
+    //Get input data
+        
+    //Add item to budget controller
+        
+    //display to UI controller
+        
+    // calculate the budget
+    
+    // Display the budget   
+        
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        if(event.keycode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+    });
+
 })(budgetController,UIController);
+
+
+
+
+
