b702baea1f822148edee62cfeaecea98aa633c36
Oliver <ors@oliver-stott.co.uk>
2018-07-30 19:22:24 +0100

    Created query selectors to grab input values, added class strings to an object called DOMstrings, can now print to the console an object with expense data

--- starter/app.js
+++ starter/app.js
@@ -5,17 +5,39 @@ var budgetController = (function (){
 
 //UI CONTROLLER
 var UIController = (function (){
-    //Code
+
+    var DOMstrings = {
+      inputType: '.add__type',
+      inputDescription: '.add__description',
+      inputValue: '.add__value',
+      inputButton: '.add__btn'
+    };
+    
+    return {
+        getInput: function(){
+            return {
+            type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+            description: document.querySelector(DOMstrings.inputDescription).value,
+            value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
+        getDOMstrings: function(){
+        return DOMstrings;
+    }
+    
+    };
+
 })();
 
 //GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl){
    
-        
+var DOM = UICtrl.getDOMstrings();
 var ctrlAddItem = function(){
     
     //Get input data
-        
+    var input = UICtrl.getInput();
+    console.log(input);
     //Add item to budget controller
         
     //display to UI controller
@@ -23,10 +45,10 @@ var ctrlAddItem = function(){
     // calculate the budget
     
     // Display the budget   
-        
+         
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
         if(event.keycode === 13 || event.which === 13){
