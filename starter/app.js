2e3230d749343f3663ce86bd26335468fa315131
Oliver <ors@oliver-stott.co.uk>
2018-08-01 17:31:12 +0100

    update to data structure

--- starter/app.js
+++ starter/app.js
@@ -3,17 +3,15 @@ var budgetController = (function (){
     
     var Expense = function(id, description, value){
         this.id = id;
-        this.description = descriptionl
+        this.description = description;
         this.value= value;
     };
     
     var Income = function(id, description, value){
         this.id = id;
-        this.description = descriptionl
+        this.description = description;
         this.value= value;
     };
-    
-    
     
     var data = {
         allItems: {
@@ -26,8 +24,36 @@ var budgetController = (function (){
         }
         
     };
+    
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+            
+            //  create new id
+            if (data.allItems[type].length > 0){
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+               } else {
+                   ID = 0;
+               }
+            
+            //create new item based on inc or exp type
+            if(type === 'exp'){
+                newItem = new Expense(ID, des, val);
 
-})();
+            } else if (type === 'inc'){
+                newItem = new Income(ID, des, val);
+            }
+            
+            //push it into our data structure
+            data.allItems[type].push(newItem);
+            
+            //return the new element
+            return newItem;
+        }
+        
+    }
+
+})(); //End of budgetController IIFE
 
 //UI CONTROLLER
 var UIController = (function (){
@@ -37,7 +63,7 @@ var UIController = (function (){
       inputDescription: '.add__description',
       inputValue: '.add__value',
       inputButton: '.add__btn'
-    };
+    }
     
     return {
         getInput: function(){
@@ -45,7 +71,7 @@ var UIController = (function (){
             type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
             description: document.querySelector(DOMstrings.inputDescription).value,
             value: document.querySelector(DOMstrings.inputValue).value
-            }
+            };
         },
         getDOMstrings: function(){
         return DOMstrings;
@@ -73,10 +99,12 @@ var controller = (function (budgetCtrl, UICtrl){
 var ctrlAddItem = function(){
     
     //Get input data
-    var input = UICtrl.getInput();
-    console.log(input);
+    var input, newItem;
+    
+    input = UICtrl.getInput();
+    
     //Add item to budget controller
-        
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);    
     //display to UI controller
         
     // calculate the budget
